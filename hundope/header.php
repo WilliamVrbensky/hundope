17c5033d96f261a9d74cd9adb7369b6debc5991a
WilliamVrbensky <BillyVrbensky@utmuser152-49.wireless.utoronto.ca>
2015-03-02 17:00:47 -0500

    added favicon to our webpage

--- hundope/header.php
+++ hundope/header.php
@@ -5,10 +5,12 @@
  * Displays all of the <head> section and everything up till <div id="content">
  *
  * @package hundope
- */
+ *The code refered to as link rel="shortcut icon" below the head represents a custom favicon on the page tab left to our website name, visualized as a hand that we created.
+**/
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico" />
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
