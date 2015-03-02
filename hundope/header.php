eff97eafc02e7f0c08b6ef3fad1837461c03976d
WilliamVrbensky <BillyVrbensky@utmuser152-49.wireless.utoronto.ca>
2015-03-02 16:52:29 -0500

    header changes

--- hundope/header.php
+++ hundope/header.php
@@ -20,17 +20,32 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'hundope' ); ?></a>
-
+	
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+
+	<div class="site-branding">
+			<div class="title-box">
+			<div id="gradient">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
-
+		</div>
+		</div>
+		</div>
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'hundope' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+			<div class="search-toggle">
+    			<i class="fa fa-search"></i>
+    			<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'hundope' ); ?></a>
+			</div>
+		</nav>
+		
+		<div id="search-container" class="search-box-wrapper clear">
+    <div class="search-box clear">
+        <?php get_search_form(); ?>
+    </div>
+    </div> 
+		
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
