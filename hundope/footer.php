3c28b80470610fb9779fdf7ff9219152c7246b02
WilliamVrbensky <BillyVrbensky@utmuser152-137.wireless.utoronto.ca>
2015-03-04 13:56:08 -0500

    footer edits

--- hundope/footer.php
+++ hundope/footer.php
@@ -5,21 +5,21 @@
  * Contains the closing of the #content div and all content after
  *
  * @package hundope
+ * i class displays copyright logo to the left of Safeguard Security in the footer.
+ * hundope_social_menu is the code snippet to allow the placement of the social media menu in the footer.
+ * get_sidebar function calls our side-footer.php file to display the footer widgets.
  */
 ?>
-
 	</div><!-- #content -->
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<?php get_sidebar('footer'); ?>
 		<div class="site-info">
-			<i class="fa fa-copyright"></i>
-			<?php printf( __( 'Safeguard Security', 'hundope' ), 'Safeguard Security' ); ?>
+		<i class="fa fa-copyright"></i>
+		<?php printf( __( 'Safeguard Security', 'hundope' ), 'Safeguard Security' ); ?>
 		</div><!-- .site-info -->
 		<?php hundope_social_menu(); ?>
 	</footer>
 </div>
 
 <?php wp_footer(); ?>
-
-</body>
 </html>
