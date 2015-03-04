df378a70ca43c6a71e607c08dc744fd5ec1bd8f3
WilliamVrbensky <BillyVrbensky@utmuser152-137.wireless.utoronto.ca>
2015-03-04 13:34:50 -0500

    added footer widget area to wordpress and comments about it

--- hundope/functions.php
+++ hundope/functions.php
@@ -80,21 +80,32 @@ endif; // hundope_setup
 add_action( 'after_setup_theme', 'hundope_setup' );
 
 /**
- * Register widget area.
- *
- * @link http://codex.wordpress.org/Function_Reference/register_sidebar
+ * The code below registers the sidebar and footer widget area.
+ * Sidebar Widgets shows up under the WordPress dashboard to drag and drop widgets into the area.
+ * Footer Widgets shows up under the WordPress dashboard to drag and drop widgets into the area.
  */
 function hundope_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'hundope' ),
+		'name'          => __( 'Sidebar Widgets', 'hundope' ),
 		'id'            => 'sidebar-1',
-		'description'   => '',
+		'description'   => 'Sidebar widgets area on the sidebar.',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
+	
+	register_sidebar( array(
+		'name'          => __( 'Footer Widgets', 'hundope' ),
+		'description'   => __( 'Footer widgets area on the footer.', 'hundope'),
+		'id'            => 'sidebar-2',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
 }
+
 add_action( 'widgets_init', 'hundope_widgets_init' );
 
 /**
